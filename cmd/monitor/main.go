89a2bcbd4447e54e67074e5b5e37edfe413e448a
Martin Hutchinson <mhutchinson@gmail.com>
2021-06-30 12:58:28 +0100

    Added a monitor that follows the log, printing out verified releases.
    This monitor persists a checkpoint locally in order to maintain a consistent view of the log's history between runs. For each leaf:
     * The raw data is downloaded into memory
     * The leaf is verified to be included within the golden checkpoint
     * XXX: The trailing newlines are removed from the leaf data
     * The leaf data is parsed as a note, and confirmed to be signed by the expected key
     * The note text is parsed as a FirmwareRelease
     * Some useful subset of data from the verified release is printed to the log
    
    The monitor will detect new checkpoints by polling the log periodically.
    Any error will cause the tool to fail; we may wish to make some failures print warnings/errors in the log instead.
    On succesfully verifying all leaves up to a checkpoint size, the checkpoint is persisted locally so that the visited leaves will not be visited again.

---
+++ cmd/monitor/main.go
@@ -0,0 +1,218 @@
+// Copyright 2021 The Project Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// monitor starts a long-running process that will continually follow a log
+// for new checkpoints. All checkpoints are checked for consistency, and all
+// leaves in the tree will be downloaded, verified, and the release info
+// printed to the program's log (via `glog.Info`).
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/f-secure-foundry/armory-drive-log/api"
+	"github.com/golang/glog"
+	"github.com/google/trillian-examples/serverless/client"
+	"github.com/google/trillian/merkle/rfc6962/hasher"
+	"golang.org/x/mod/sumdb/note"
+)
+
+var (
+	pollInterval  = flag.Duration("poll_interval", 1*time.Minute, "The interval at which the log will be polled for new data")
+	stateFile     = flag.String("state_file", "", "File path for where checkpoints should be stored")
+	logURL        = flag.String("log_url", "https://raw.githubusercontent.com/f-secure-foundry/armory-drive-log/master/log/", "URL identifying the location of the log")
+	logPubKey     = flag.String("log_pubkey", "armory-drive-log-test+a5aae457+AbDoiIsZgSk5H0v0LjKPKv5dAMb0IfB47tocFtGmyW44", "The log's public key")
+	releasePubKey = flag.String("release_pubkey", "armory-drive-test+e0b83da5+ARFd7yMO7VQgK/N+KWETnS5O6dSqcTmTzQUXgQhJVVG0", "The release signer's public key")
+)
+
+func main() {
+	flag.Parse()
+	ctx := context.Background()
+
+	st, isNew, err := stateTrackerFromFlags()
+	if err != nil {
+		glog.Exitf("Failed to create new LogStateTracker: %v", err)
+	}
+
+	var releaseVerifiers note.Verifiers
+	if v, err := note.NewVerifier(*releasePubKey); err != nil {
+		glog.Exitf("Failed to construct release note verifier: %v", err)
+	} else {
+		releaseVerifiers = note.VerifierList(v)
+	}
+
+	monitor := Monitor{
+		st:               st,
+		stateFile:        *stateFile,
+		releaseVerifiers: releaseVerifiers,
+		handler: func(i uint64, r api.FirmwareRelease) error {
+			glog.Infof("%d: Release version %q available at %s", i, r.Revision, r.SourceURL)
+			return nil
+		},
+	}
+
+	if isNew {
+		// This monitor has no memory of running before, so let's catch up with the log.
+		if err := monitor.From(0); err != nil {
+			glog.Exitf("monitor.From(%d): %v", 0, err)
+		}
+	}
+
+	// We've processed all leaves committed to by the tracker's checkpoint, and now we enter polling mode.
+	ticker := time.NewTicker(*pollInterval)
+	defer ticker.Stop()
+	for {
+		lastHead := st.LatestConsistent.Size
+		if err := st.Update(); err != nil {
+			glog.Exitf("Failed to update checkpoint: %q", err)
+		}
+		if st.LatestConsistent.Size > lastHead {
+			glog.V(1).Infof("Found new checkpoint for tree size %d, fetching new leaves", st.LatestConsistent.Size)
+			if err := monitor.From(lastHead); err != nil {
+				glog.Exitf("monitor.From(%d): %v", lastHead, err)
+			}
+		} else {
+			glog.V(2).Infof("Polling: no new data found; tree size is still %d", st.LatestConsistent.Size)
+		}
+
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+			// Go around the loop again.
+		}
+	}
+}
+
+// Monitor verifiably checks inclusion of all leaves in a range, and then passes the
+// parsed FirmwareRelease to a handler.
+type Monitor struct {
+	st               client.LogStateTracker
+	stateFile        string
+	releaseVerifiers note.Verifiers
+	handler          func(uint64, api.FirmwareRelease) error
+}
+
+// From checks the leaves from `start` up to the checkpoint from the state tracker.
+// Upon reaching the end of the leaves, the checkpoint is persisted in the state file.
+func (m *Monitor) From(start uint64) error {
+	fromCP := m.st.LatestConsistent
+	pb, err := client.NewProofBuilder(fromCP, m.st.Hasher.HashChildren, m.st.Fetcher)
+
+	if err != nil {
+		return fmt.Errorf("failed to construct proof builder: %v", err)
+	}
+	for i := start; i < fromCP.Size; i++ {
+		rawLeaf, err := client.GetLeaf(m.st.Fetcher, i)
+		if err != nil {
+			return fmt.Errorf("failed to get leaf at index %d: %v", i, err)
+		}
+		hash := m.st.Hasher.HashLeaf(rawLeaf)
+		ip, err := pb.InclusionProof(i)
+		if err != nil {
+			return fmt.Errorf("failed to get inclusion proof for index %d: %v", i, err)
+		}
+		wantRoot, err := m.st.Verifier.RootFromInclusionProof(int64(i), int64(fromCP.Size), ip, hash)
+		if err != nil {
+			return fmt.Errorf("failed to calculate root from inclusion proof at index %d: %v", i, err)
+		}
+		if !bytes.Equal(wantRoot, fromCP.Hash) {
+			return fmt.Errorf("failed to verify inclusion proof for leaf %d.\ncheckpoint: %x\ncalculated: %x",
+				i, fromCP.Hash, wantRoot)
+		}
+
+		// XXX: this is unexpected. the hash is over the data with the trailing newline but we need to remove it
+		// in order to make this data into a valid note.
+		trimmedLeaf := strings.TrimSuffix(string(rawLeaf), "\n")
+		releaseNote, err := note.Open([]byte(trimmedLeaf), m.releaseVerifiers)
+		if err != nil {
+			return fmt.Errorf("failed to open leaf note at index %d: %v", i, err)
+		}
+
+		var release api.FirmwareRelease
+		if err := json.Unmarshal([]byte(releaseNote.Text), &release); err != nil {
+			return fmt.Errorf("failed to unmarshal release at index %d: %w", i, err)
+		}
+		if err := m.handler(i, release); err != nil {
+			return fmt.Errorf("handler(): %w", err)
+		}
+	}
+	return ioutil.WriteFile(m.stateFile, m.st.LatestConsistentRaw, 0644)
+}
+
+// stateTrackerFromFlags constructs a state tracker based on the flags provided to the main invocation.
+// The checkpoint returned will be the checkpoint representing this monitor's view of the log history.
+// A boolean is returned that is true iff the checkpoint was fetched from the log to initialize state.
+func stateTrackerFromFlags() (client.LogStateTracker, bool, error) {
+	if len(*stateFile) == 0 {
+		glog.Exit("--state_file required")
+	}
+
+	state, err := ioutil.ReadFile(*stateFile)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			glog.Infof("State file %q missing. Will trust first checkpoint received from log.", *stateFile)
+		} else {
+			return client.LogStateTracker{}, false, fmt.Errorf("could not read state file %q: %w", *stateFile, err)
+		}
+	}
+
+	root, err := url.Parse(*logURL)
+	if err != nil {
+		return client.LogStateTracker{}, false, fmt.Errorf("failed to parse log URL %q: %w", *logURL, err)
+	}
+	f, err := newFetcher(root)
+	if err != nil {
+		return client.LogStateTracker{}, false, fmt.Errorf("failed to create fetcher: %v", err)
+	}
+
+	lSigV, err := note.NewVerifier(*logPubKey)
+	if err != nil {
+		return client.LogStateTracker{}, false, fmt.Errorf("unable to create new log signature verifier: %w", err)
+	}
+
+	lst, err := client.NewLogStateTracker(f, hasher.DefaultHasher, state, lSigV)
+	return lst, state == nil, err
+}
+
+// newFetcher creates a Fetcher for the log at the given root location.
+func newFetcher(root *url.URL) (client.Fetcher, error) {
+	if s := root.Scheme; s != "http" && s != "https" {
+		return nil, fmt.Errorf("unsupported URL scheme %s", s)
+	}
+
+	return func(p string) ([]byte, error) {
+		u, err := root.Parse(p)
+		if err != nil {
+			return nil, err
+		}
+		resp, err := http.Get(u.String())
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+		return ioutil.ReadAll(resp.Body)
+	}, nil
+}
